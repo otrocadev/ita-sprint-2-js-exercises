27d2dbeedb1f2c776b85f5c5ecf8ca56728997f8
Ot Roca <otrocadev@gmail.com>
2025-07-09 14:09:37 +0200

    All folders and files that the project will have created

---
+++ 07-promises/promises.js
@@ -0,0 +1,15 @@
+// HTML interactions
+const exercise1Btn = document.getElementById('exercise1-btn')
+exercise1Btn.addEventListener('click', (e) => {})
+
+const exercise2Btn = document.getElementById('exercise2-btn')
+exercise2Btn.addEventListener('click', (e) => {})
+
+const exercise31Btn = document.getElementById('exercise31-btn')
+exercise31Btn.addEventListener('click', (e) => {})
+
+const exercise32Btn = document.getElementById('exercise32-btn')
+exercise32Btn.addEventListener('click', (e) => {})
+
+const exercise4Btn = document.getElementById('exercise4-btn')
+exercise4Btn.addEventListener('click', (e) => {})
