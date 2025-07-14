3784189fca4748e4bcb97a8431d59c7b1abef8df
Ot Roca <otrocadev@gmail.com>
2025-07-14 12:50:15 +0200

    Array loop exercises completed

--- 06-array-loops/array-loops.js
+++ 06-array-loops/array-loops.js
@@ -1,15 +1,46 @@
+let names = ['Anna', 'Bernat', 'Clara']
+
+let numbers = [1, 2, 3, 4, 5, 6]
+
+let obj = {
+  nom: 'Ona',
+  edat: 25,
+  ciutat: 'Barcelona',
+}
+
 // HTML interactions
 const exercise1Btn = document.getElementById('exercise1-btn')
-exercise1Btn.addEventListener('click', (e) => {})
+exercise1Btn.addEventListener('click', (e) => {
+  names.forEach((name) => console.log(name))
+})
 
 const exercise2Btn = document.getElementById('exercise2-btn')
-exercise2Btn.addEventListener('click', (e) => {})
+exercise2Btn.addEventListener('click', (e) => {
+  for (const element of names) console.log(element)
+})
 
-const exercise31Btn = document.getElementById('exercise31-btn')
-exercise31Btn.addEventListener('click', (e) => {})
-
-const exercise32Btn = document.getElementById('exercise32-btn')
-exercise32Btn.addEventListener('click', (e) => {})
+const exercise3Btn = document.getElementById('exercise3-btn')
+exercise3Btn.addEventListener('click', (e) => {
+  const evenNumbers = numbers.filter((evenNumber) => evenNumber % 2 === 0)
+  console.log(evenNumbers)
+})
 
 const exercise4Btn = document.getElementById('exercise4-btn')
-exercise4Btn.addEventListener('click', (e) => {})
+exercise4Btn.addEventListener('click', (e) => {
+  for (const key in obj) console.log(key + ': ' + obj[key])
+})
+
+const exercise5Btn = document.getElementById('exercise5-btn')
+exercise5Btn.addEventListener('click', (e) => {
+  for (const number of numbers) {
+    if (number === 5) break
+    console.log(number)
+  }
+})
+
+const exercise6Btn = document.getElementById('exercise6-btn')
+exercise6Btn.addEventListener('click', (e) => {
+  for (const name of names) {
+    console.log(names.indexOf(name) + ': ' + name)
+  }
+})
